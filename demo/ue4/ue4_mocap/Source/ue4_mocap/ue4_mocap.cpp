96f36b7e3dea542625953a4af4137360f4aedba6
cachtayl <125492776+cachtayl-peeldev@users.noreply.github.com>
2023-02-21 19:46:50 -0800

    setup

--- demo/ue4/ue4_mocap/Source/ue4_mocap/ue4_mocap.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "ue4_mocap.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ue4_mocap, "ue4_mocap" );
