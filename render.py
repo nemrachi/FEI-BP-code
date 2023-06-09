7b1c0fe3bd38efabde7c8535b6be8addd2637e1b
Ema Richnakova <ema.richnakova@gmail.com>
2023-06-09 12:32:46 +0200

    refactor and live data render nearly done

--- render.py
+++ render.py
@@ -3,10 +3,13 @@ import matplotlib.animation as animation
 import matplotlib.pyplot as pyplot
 import numpy as np
 import re
-from transforms3d.euler import euler2mat, mat2euler
+import socket
+from transforms3d.euler import euler2mat
 
+# EXAMPLE HOW TO RUN SCRIPT
 # py render.py --bvh=./resources/zdvihnutie_p_ruky_chr01.bvh
 
+##JOINT#CLASS###################################################################
 class Joint:
     def __init__(self, index, name, parent):
         self.index = index # BFS hierarchy structure position
@@ -15,10 +18,10 @@ class Joint:
         self.children = []
         self.offset = np.zeros(3)
         self.channels = []
-        self.motion = [] # position of each frame
-        
+        self.motion = [] # motion of each frame
+##JOINT#CLASS###################################################################
 
-
+##SKELETON#CLASS################################################################
 class Skeleton:
     def __init__(self):
         self.root = None # root joint, first in hierarchy
@@ -75,21 +78,22 @@ class Skeleton:
                 self.frame_num = int(words[1])
             elif line.startswith('Frame Time'):
                 self.frame_time = float(words[2])
-            else: # get action matrix
+            else: # get save motions to matrix rows
                 frame = [float(word) for word in words]
                 self.frame_matrix.append(frame)
-        self.frame_matrix = np.array(self.frame_matrix) # turn into numpy array
+        self.frame_matrix = np.array(self.frame_matrix)
 
     # calculate the position of each joint for each frame
-    def get_all_positions(self):
+    def get_all_positions(self, frame_num=None):
         global motion_index
-        for i in range(self.frame_num):
+        frame_num = frame_num or self.frame_num
+        for i in range(frame_num):
             motion_index = 0
             self.get_joint_positions(i, self.root, np.eye(3), np.zeros(3))
             # eye = 2-D array with ones on the diagonal and zeros elsewhere
 
     # recursively calculate position of given joint in given frame(index)
-    # source: https://research.cs.wisc.edu/graphics/Courses/cs-838-1999/Jeff/BVH.html
+    # resource: https://research.cs.wisc.edu/graphics/Courses/cs-838-1999/Jeff/BVH.html
     def get_joint_positions(self, frame_index, joint, parent_matrix, parent_position):
         global motion_index
         # find value corresponding to given joint of given frame in frame_matrix
@@ -101,7 +105,8 @@ class Skeleton:
                     rotation = self.frame_matrix[frame_index, motion_index]
                     rotation = np.deg2rad(rotation)
                     euler_rotation = np.array([rotation, 0., 0.]) # floats
-                    M = euler2mat(*euler_rotation) # rotation matrix from Euler angles and axis sequence
+                    # rotation matrix from Euler angles and axis sequences
+                    M = euler2mat(*euler_rotation)
                     matrix = matrix.dot(M) # dot product of arrays
                 case 'Yrotation':
                     rotation = self.frame_matrix[frame_index, motion_index]
@@ -126,7 +131,7 @@ class Skeleton:
         new_position = parent_position + parent_matrix.dot(joint.offset) + offset
         new_matrix = parent_matrix.dot(matrix)
         joint.motion.append(new_position)
-        # calculate position of child node
+        # calculate position of child nodes
         for c in joint.children:
             self.get_joint_positions(frame_index, c, new_matrix, new_position)
 
@@ -139,16 +144,56 @@ class Skeleton:
             position_b = skeleton.joints[joint_b].motion
             bone_data.append([position_a, position_b])
         bone_data = np.array(bone_data)
+        return bone_data
+##SKELETON#CLASS################################################################
 
-# update animation
-def update(num, lines, boneDatas):
-    for line,data in zip(lines,boneDatas):
+##ANIMATION#FUNCTIONS###########################################################
+# update animation from bvh file
+def update(num, lines, bone_data):
+    for line, data in zip(lines,bone_data):
+        line.set_data([data[:, num, 0], data[:, num, 2]]) # first dimension = 2 - refers to the 2 vertices of each edge
+        line.set_3d_properties(data[:, num, 1])
+    return lines
+
+# update animation from bvh broadcasting
+def update_live_data(num):
+    global ax        
+    skeleton.frame_matrix = skeleton.frame_matrix + get_live_motion_frame()
+    skeleton.get_all_positions(1)
+    bone_data = skeleton.get_bone_data()
+    # in bvh file, Y and Z axes are reversed, so exchange (XYZ)
+    lines = [ax.plot([bone[0,0,0], bone[1,0,0]], 
+                [bone[0,0,2],bone[1,0,2]],
+                [bone[0,0,1],bone[1,0,1]])[0] for bone in bone_data]
+    for line, data in zip(lines, bone_data):
         line.set_data([data[:, num, 0], data[:, num, 2]]) # first dimension is 2: refers to the two vertices of each edge
         line.set_3d_properties(data[:, num, 1])
     return lines
 
+# parse motion frame from socket data
+def get_live_motion_frame():
+    global bvh_socket
+    # remove metadata from motion frame
+    motion_frame = bvh_socket.recv(3000).decode("utf-8")[15:-3]
+    motion_frame = np.fromstring(motion_frame, dtype=float, sep=' ')
+    without_meta = []
+    n = 3 # slice frame to lists of 3 numbers 
+    motion_frame = [motion_frame[i:i + n] for i in range(0, len(motion_frame), n)]
+    # fisrt six numbers and then every second number trio are actual data
+    for i, trio in enumerate(motion_frame):
+        if i in (0,1) or i % 2:
+            without_meta.extend(trio)
+    return np.array([without_meta])
+##ANIMATION#FUNCTIONS###########################################################
+
+##GLOBAL########################################################################
+UDP_IP = '127.0.0.1'
+UDP_PORT_EDIT = 7001
+UDP_PORT_CAPTURE = 7002
+
 parser = argparse.ArgumentParser()
-parser.add_argument('--bvh', type=str, required=False, help='path to bvh file to render', default='./resources/zdvihnutie_p_ruky_chr01.bvh')
+parser.add_argument('--bvh', type=str, required=False, help='path to bvh file to render', default='./basic_hierarchy.bvh')
+##GLOBAL########################################################################
 
 if __name__ == '__main__':
     # check and read bvh file
@@ -162,28 +207,35 @@ if __name__ == '__main__':
     skeleton = Skeleton()
     skeleton.parse(lines)
 
-    if skeleton.frame_num: # file
+    # animate
+    fig = pyplot.figure()
+    global ax
+    ax = fig.add_subplot(111, projection='3d')
+    ax.set_xlim(-100, 100)
+    ax.set_ylim(-100, 100)
+    ax.set_zlim(-1, 199)
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    pyplot.axis('off')
+
+    if skeleton.frame_num > 1: # bvh file
         skeleton.get_all_positions()
-
         bone_data = skeleton.get_bone_data()
+        # in bvh file, Y and Z axes are reversed, so exchange (XYZ)
+        lines = [ax.plot([bone[0,0,0], bone[1,0,0]], 
+                        [bone[0,0,2],bone[1,0,2]],
+                        [bone[0,0,1],bone[1,0,1]])[0] for bone in bone_data]
+        anim = animation.FuncAnimation(fig, update, skeleton.frame_num, 
+                interval=skeleton.frame_time, blit=True, fargs=(lines, bone_data))
+        pyplot.show()
 
-        # animate
-        fig = pyplot.figure()
-        axes = fig.add_subplot(111, projection='3d')
-        axes.set_xlim(-100, 100)
-        axes.set_ylim(-100, 100)
-        axes.set_zlim(-1, 199)
-        axes.set_xlabel('X')
-        axes.set_ylabel('Y')
-        axes.set_zlabel('Z')
-        pyplot.axis('off')
-        # in bvh file, Y and Z axes are reversed, so exchange (YXZ)
-        lines = [axes.plot([boneData[0,0,0], boneData[1,0,0]], 
-                        [boneData[0,0,2],boneData[1,0,2]],
-                        [boneData[0,0,1],boneData[1,0,1]])[0] for boneData in bone_data]
-        anim = animation.FuncAnimation(fig, update,
-                skeleton.frame_num, interval=skeleton.frame_time, blit=True, fargs=(lines, bone_data))
+    else: # real-time bvh data
+        global bvh_socket
+        bvh_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        bvh_socket.bind((UDP_IP, UDP_PORT_EDIT))
+        
+        anim = animation.FuncAnimation(fig, update_live_data, frames=1,
+                interval=skeleton.frame_time, blit=True)
         # anim.save('motion.mp4')
         pyplot.show()
-    else: # real-time data
-        pass
